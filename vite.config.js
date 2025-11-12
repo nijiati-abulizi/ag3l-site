bcf8bf7cfac1ce307361118cf50c42667ac432c6
Nijat27 <nabliz@student.ubc.ca>
2025-11-12 15:17:43 -0800

    fix: typo in equipe.html

--- vite.config.js
+++ vite.config.js
@@ -1,2 +1,32 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-export default defineConfig({ server:{ open:true }, build:{ outDir:'dist' } })
+import { resolve } from 'path'
+
+export default defineConfig({
+  server: {
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        // French pages
+        index: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        who: resolve(__dirname, 'who.html'),
+        services: resolve(__dirname, 'services.html'),
+        equipe: resolve(__dirname, 'equipe.html'),
+        contact: resolve(__dirname, 'contact.html'),
+
+        // English pages
+        en_index: resolve(__dirname, 'en/index.html'),
+        en_about: resolve(__dirname, 'en/about.html'),
+        en_who: resolve(__dirname, 'en/who.html'),
+        en_services: resolve(__dirname, 'en/services.html'),
+        en_team: resolve(__dirname, 'en/team.html'),
+        en_contact: resolve(__dirname, 'en/contact.html')
+      }
+    }
+  }
+})
+
