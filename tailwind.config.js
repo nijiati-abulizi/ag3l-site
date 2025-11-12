49bb240c62401e869e2d604f9e56098d0ab0e547
Nijiati_Abulizi <114438964+nijiati-abulizi@users.noreply.github.com>
2025-11-12 12:42:01 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,2 @@
+/** @type {import('tailwindcss').Config} */
+export default {content:['./index.html','./about.html','./who.html','./services.html','./equipe.html','./contact.html','./en/**/*.html','./src/**/*.{js,ts}'],theme:{extend:{colors:{brand:{50:'#F3F7FC',100:'#E6EEF8',300:'#B7CCE9',700:'#001C44',800:'#00173A'}},boxShadow:{'xl-soft':'0 30px 60px -24px rgba(2,6,23,.25)'}}},plugins:[]}
